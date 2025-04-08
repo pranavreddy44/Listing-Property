346a1301f7b255c0d1eb1c92c788e68d1437ca8c
PranavReddy <pranavkonda12@gmail.com>
2025-04-08 23:48:02 +0530

    Tested the Listing Model with sample input

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const Listing = require("./models/listing.js");
 
 const mongo_url = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -18,6 +19,20 @@ async function main() {
 app.get("/", (req, res) => {
   res.send("Hi I am root");
 });
+
+app.get("/testListing", async (req, res) => {
+  let sample = new Listing({
+    title: "My new villa",
+    description: "By the beach",
+    price: 1200,
+    location: "Hyderbad India",
+    country: "India",
+  });
+  await sample.save();
+  console.log("Sample was saved");
+  res.send("Successful testing");
+});
+
 app.listen(3000, () => {
   console.log("Server is listening to port 3000");
 });
