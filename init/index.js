1c21771e1adf0605ea4da8ec5e2537ddb1dc9508
PranavReddy <pranavkonda12@gmail.com>
2025-08-02 23:39:47 +0530

    All files

--- init/index.js
+++ init/index.js
@@ -1,25 +1,28 @@
+if(process.env.NODE_ENV != "production") {
+    require('dotenv').config();
+}
+
 const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const dbUrl = process.env.ATLASDB_URL;
 
-main()
-  .then(() => {
+main().then(() => {
     console.log("connected to DB");
-  })
-  .catch((err) => {
+}).catch((err) => {
     console.log(err);
-  });
+});
 
 async function main() {
-  await mongoose.connect(MONGO_URL);
+    await mongoose.connect(dbUrl);
 }
 
 const initDB = async () => {
-  await Listing.deleteMany({});
-  await Listing.insertMany(initData.data);
-  console.log("data was initialized");
-};
+    await Listing.deleteMany({});
+    initData.data = initData.data.map((obj) => ({...obj, owner: "65f498de938bbbafdcf350b8"}))
+    await Listing.insertMany(initData.data);
+    console.log("data was initialized");
+}
 
-initDB();
+// initDB();
